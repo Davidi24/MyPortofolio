17f672b4eb8ea40b8d95580b3b08e97f809e1f31
David24 <dkeci21@epoka.edu.al>
2025-07-20 16:27:32 +0200

    tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EntryPage from "./pages/EntryPage";
 import me from "./assets/Icons/me.png"
+import { sendMessage } from "./utilities/Helpers/ExternalConnection";
+import { useEffect } from "react";
 
 
-const Prova = ()=>{
-  return(
+const Prova = () => {
+  return (
     <>
-    <img src={me} alt="" className="h-[40rem]" />
+      <img src={me} alt="" className="h-[40rem]" />
     </>
   )
 }
@@ -14,6 +16,19 @@ const Prova = ()=>{
 
 
 function App() {
+
+  useEffect(() => {
+    if (!sessionStorage.getItem('websiteOpened')) {
+      sendMessage({
+        fullName: "CV Download Notification",
+        email: "kecidavid22@yourdomain.com",
+        phone: "+491234567890",
+        subject: "Website opened",
+        message: "Someone opened your website",
+      });
+      sessionStorage.setItem('websiteOpened', 'true');
+    }
+  }, []);
 
   return (
     <BrowserRouter>
